48c09dda482091d832aa5b0d74024cc9de0d8691
2017kartik <kartikagarwal9119063420@gmail.com>
2025-01-09 22:28:10 +0530

    Adding the layout of Work Experience Section.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
   return (
-    <section className="relative w-full h-screen mx-auto mb-32">
+    <section className="relative w-full h-screen mx-auto mb-28 sm:24">
       <div
         className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
       >
